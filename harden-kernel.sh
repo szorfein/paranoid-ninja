cda04fd079467376a572bdef087dae2fe3612e68
szorfein <szorfein@protonmail.com>
2019-01-14 22:00:47 +0100

    wrap kernel.sh to paranoid.sh

--- harden-kernel.sh
+++ harden-kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+FILE="$1.txt"
+#DIR=/usr/src/linux-4.19.9-gentoo/
 DIR=/usr/src/linux
 CONF=$DIR/.config
 OLD=$(pwd)
-#CONF=/usr/src/linux/.config
-#CONF=~/ttt
 
 # colors
 red=$'\e[0;91m'
@@ -27,17 +27,17 @@ updConf() {
 
 # Return the regex for the sed command from applyRule()
 retRule() {
-  local s clean o rule old
+  local s clean q rule old
   s=$1
   clean=${s%=*}
-  o=${s#*=}
+  q=${s#*=}
   old=$(grep -ie "$clean" $CONF | head -n 1)
-  [[ -z $old ]] && die "Option '$s' no found"
-  if [ $o == "n" ]; then
+  [[ -z $old ]] && return
+  if [ $q == "n" ]; then
     rule="s:${old}:${clean}=n:g"
-  elif [ $o == "y" ] ; then
+  elif [ $q == "y" ] ; then
     rule="s:${old}:${clean}=y:g"
-  elif [ $o == "m" ] ; then
+  elif [ $q == "m" ] ; then
     rule="s:${old}:${clean}=m:g"
   else
     rule="s:${old}:${s}:g"
@@ -57,9 +57,18 @@ applyRules() {
 }
 
 chkOption() {
+  local s clean q not_set is_void
+  s=$1
+  clean=${s%=*}
+  q=${s#*=}
+  not_set=$(grep -iE "^# $clean is not set" $CONF)
+  is_void=$(grep -i $clean $CONF)
   if grep $1 $CONF >/dev/null; then
-    printf "${cyan}%s${white}%s${endc}\n" \
-      "[OK]" " $1"
+    printf "${cyan}%s${white}%s${endc}\n" "[OK]" " $1"
+  elif [[ $q == n ]] && [[ $not_set ]] ; then
+    printf "${cyan}%s${white}%s${endc}\n" "[OK]" " $1"
+  elif [[ -z $is_void ]] ; then
+    printf "${red}%s${endc}\n" "Option $s no found..."
   else
     applyRules $1
   fi
@@ -86,6 +95,9 @@ check_root() {
 
 check_root
 
+# Check arg
+[[ ! -f src/$FILE ]] && die "config not available"
+
 # Check if /usr/src/linux exist
 if [ ! -s /usr/src/linux ] ; then
   die "Link of /usr/src/linux no found, pls create one"
@@ -100,15 +112,18 @@ if [ ! -f $CONF ] ; then
 fi
 
 # Kernel options to check :)
-for config in $(grep -ie "^config" $OLD/src/harden.txt) ; do
+for config in $(grep -ie "^config" $OLD/src/$FILE) ; do
   chkOption "$config"
 done
 
-forIntel
+# Add special configs by CPU
+if [ $FILE == "harden.txt" ] ; then
+  forIntel
+fi
 
 # clean work
 #echo -e "\n[*] Clean kernel config." 
 #make mrproper >/dev/null
 
-echo -e "\n[*] Your kernel is hardened." 
+echo -e "\n[*] $FILE has beed apply :)." 
 exit 0
